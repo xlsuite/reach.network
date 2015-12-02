330cf09bceec691ef2b40ad57af2949501ab74cb
Igor Luzhanov <luzhanov@gmail.com>
2015-12-02 18:22:51 +0100

    Chromeapp - formatting

--- player-chromeapp/app/js/message-window.js
+++ player-chromeapp/app/js/message-window.js
@@ -1,5 +1,6 @@
-$rv.messageWindow = function(message) {
-  message = encodeURIComponent(message);
-  chrome.app.window.create("message-screen/index.html?message=" + message,
-  {state: "fullscreen"}, function() {});
+$rv.messageWindow = function (message) {
+    message = encodeURIComponent(message);
+    chrome.app.window.create("message-screen/index.html?message=" + message,
+        {state: "fullscreen"}, function () {
+        });
 };
--- player-chromeapp/app/js/player/watchdog.js
+++ player-chromeapp/app/js/player/watchdog.js
@@ -4,46 +4,46 @@
 
 rvWatchdog = function () {
 
-	var HEARTBEAT_TIMER_INTERVAL_MS = 60 * 1000;
-	var MAX_HEARTBEAT_GAP_MS = 3 * HEARTBEAT_TIMER_INTERVAL_MS;
-	
-	var lastHearbeat;
-	var callback;
-	
-	this.start = function(callback_function) {
-		console.log('Watchdog start');
-		callback = callback_function;
-		this.poke();
-		onTimer();
-	};
+    var HEARTBEAT_TIMER_INTERVAL_MS = 60 * 1000;
+    var MAX_HEARTBEAT_GAP_MS = 3 * HEARTBEAT_TIMER_INTERVAL_MS;
 
-	this.poke = function() {
-		//console.log('poke');
-		lastHearbeat = new Date();
-	};
+    var lastHearbeat;
+    var callback;
 
-	var onTimer = function() {
-		try {
-			//console.log('on timer');
-			var now = new Date();
-			if ((lastHearbeat.getTime() + MAX_HEARTBEAT_GAP_MS) < now.getTime()) {
-				HEARTBEAT_TIMER_INTERVAL_MS = (HEARTBEAT_TIMER_INTERVAL_MS * 2) + (Math.random() * 10000);
-				if (HEARTBEAT_TIMER_INTERVAL_MS > 18000000) {
-					HEARTBEAT_TIMER_INTERVAL_MS = 18000000 + (Math.random() * 10000);
-				}
-				if (callback) {
-					console.log('watchdog triggered at ' + now);
-					callback();
-				}
-				return;
-			}
-			HEARTBEAT_TIMER_INTERVAL_MS = 60 * 1000;
-		} catch (e) {
-			console.log('watchdog error: ' + e.message);
-		} finally {
-			setTimeout(onTimer, HEARTBEAT_TIMER_INTERVAL_MS);
-		}
-	};
+    this.start = function (callback_function) {
+        console.log('Watchdog start');
+        callback = callback_function;
+        this.poke();
+        onTimer();
+    };
+
+    this.poke = function () {
+        //console.log('poke');
+        lastHearbeat = new Date();
+    };
+
+    var onTimer = function () {
+        try {
+            //console.log('on timer');
+            var now = new Date();
+            if ((lastHearbeat.getTime() + MAX_HEARTBEAT_GAP_MS) < now.getTime()) {
+                HEARTBEAT_TIMER_INTERVAL_MS = (HEARTBEAT_TIMER_INTERVAL_MS * 2) + (Math.random() * 10000);
+                if (HEARTBEAT_TIMER_INTERVAL_MS > 18000000) {
+                    HEARTBEAT_TIMER_INTERVAL_MS = 18000000 + (Math.random() * 10000);
+                }
+                if (callback) {
+                    console.log('watchdog triggered at ' + now);
+                    callback();
+                }
+                return;
+            }
+            HEARTBEAT_TIMER_INTERVAL_MS = 60 * 1000;
+        } catch (e) {
+            console.log('watchdog error: ' + e.message);
+        } finally {
+            setTimeout(onTimer, HEARTBEAT_TIMER_INTERVAL_MS);
+        }
+    };
 
 };
 
